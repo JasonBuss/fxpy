a7fd7c83c5c42f1424f492ec7f6f4ac62d007579
Jason Buss <jason.buss@customerfx.com>
2016-03-20 15:58:42 -0500

    created get_sqlupdate

--- fx_sqlstrings.py
+++ fx_sqlstrings.py
@@ -18,17 +18,26 @@ def get_sqlselect(tablename, *fields):
 	sql = "select {0} from {1}".format(x, tablename).upper()
 	return sql
 	
+def get_sqlupdate(tablename, ID, fieldname, value):
+	if tablename.upper() = "OPPORTUNITY_PRODUCT":
+		x = "OPPPRODUCTID"
+	else:
+		x = tablename + "ID"
+	
+	sql = "update {0} set {1} = {2} where {3} = {4}".format(tablename, fieldname, value, x, ID)
+	return sql.upper()
+	
 def get_sqlinsert(tablename, *fields):
-	x = tablename + "ID"
+	if tablename.upper() == "OPPORTUNITY_PRODUCT":
+		x = "OPPPRODUCTID"
+	else:
+		x = tablename + "ID"
+		
+	
 	for field in fields:
 		x += ", " + field
 	return "select " + x + " from " + tablename
 
-def get_sqlupdate(tablename, *fields):
-	x = tablename + "ID"
-	for field in fields:
-		x += ", " + field
-	return "select " + x + " from " + tablename	
 
 def get_sqlwhere(tablename, condition):
 	x = tablename + "ID"
