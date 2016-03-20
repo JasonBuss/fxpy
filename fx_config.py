d15654fd6b8a7c5d7ef71d4b2e5225e9b2a44159
Jason Buss <jason.buss@customerfx.com>
2016-03-20 17:13:44 -0500

    updates to configuration script

---
+++ fx_config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# script managing config files
+# Author:	Jason Buss
+# Created:	2016-03-20
+# Version: 	1.0
+
+
+import fx_common
+	
+database = fx_common.get_config('sql.config')
